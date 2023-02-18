bf6f9e7db19bb896db59fc4891a0198cf53bc18b
Chase Harrison <chaseh88@gmail.com>
2023-02-17 22:46:42 -0600

    hook up cache

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const PORT = 4001;
 
 app.get("/", async ({ query }, res) => {
   const { latitude, longitude } = query;
-  const data = await getLocationData(latitude, longitude);
-  res.status(200).send(data);
+  const location = await getLocationData(latitude, longitude);
+  res.status(200).send({
+    location,
+    currentWeather: {
+      temperature: 70,
+      summary: "Partly Cloudy",
+    },
+  });
 });
 
 app.listen(PORT, () => {
