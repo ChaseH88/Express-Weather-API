c2c278f59781a6295a8a9f48144a22e8e5ec2d95
Chase Harrison <chaseh88@gmail.com>
2023-02-18 06:32:25 -0600

    adding API key logic, sqlite, and uuid

--- index.js
+++ index.js
@@ -1,9 +1,71 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("apikeys.db");
+
 const { getLocationData } = require("./api/mapbox");
 const { getCurrentWeatherData } = require("./api/currentweather");
 const { getFutureWeatherData } = require("./api/future-weather");
+
 const app = express();
 const PORT = 4001;
+
+db.run(`CREATE TABLE IF NOT EXISTS apikeys (email TEXT PRIMARY KEY, key TEXT)`);
+
+// Create a new endpoint to generate or retrieve API keys
+app.get("/api-key", (req, res) => {
+  const email = req.query.email.toLowerCase();
+  db.get(`SELECT * FROM apikeys WHERE email = ?`, email, (err, row) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send({
+        message: "Error retrieving API key",
+        key: null,
+      });
+    } else if (row) {
+      res.status(200).send({
+        message: "API key retrieved successfully",
+        key: row.key,
+      });
+    } else {
+      const apiKey = uuidv4();
+      db.run(
+        `INSERT INTO apikeys (email, key) VALUES (?, ?)`,
+        email,
+        apiKey,
+        (err) => {
+          if (err) {
+            console.error(err);
+            res.status(500).send({
+              message: "Error generating API key",
+              key: null,
+            });
+          } else {
+            res.status(200).send({
+              message: "API key generated successfully",
+              key: apiKey,
+            });
+          }
+        }
+      );
+    }
+  });
+});
+
+// Add middleware to verify API key on each request
+app.use((req, res, next) => {
+  const apiKey = req.headers["x-api-key"];
+  db.get(`SELECT * FROM apikeys WHERE key = ?`, apiKey, (err, row) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error verifying API key");
+    } else if (!row) {
+      res.status(401).send("Invalid API key");
+    } else {
+      next();
+    }
+  });
+});
 
 app.get("/", async ({ query }, res) => {
   const { latitude, longitude } = query;
