abe7da1ba660062cc2594f7056e7e2aff93d9d01
Chase Harrison <chaseh88@gmail.com>
2023-02-18 06:35:43 -0600

    clean up

--- index.js
+++ index.js
@@ -10,75 +10,81 @@ const { getFutureWeatherData } = require("./api/future-weather");
 const app = express();
 const PORT = 4001;
 
+// Create the apikeys table if it doesn't exist
 db.run(`CREATE TABLE IF NOT EXISTS apikeys (email TEXT PRIMARY KEY, key TEXT)`);
 
-// Create a new endpoint to generate or retrieve API keys
-app.get("/api-key", (req, res) => {
+// Create or retrieve an API key for the given email address
+app.get("/api-keys", (req, res) => {
   const email = req.query.email.toLowerCase();
   db.get(`SELECT * FROM apikeys WHERE email = ?`, email, (err, row) => {
     if (err) {
       console.error(err);
-      res.status(500).send({
+      return res.status(500).json({
         message: "Error retrieving API key",
         key: null,
       });
-    } else if (row) {
-      res.status(200).send({
+    }
+    if (row) {
+      return res.json({
         message: "API key retrieved successfully",
         key: row.key,
       });
-    } else {
-      const apiKey = uuidv4();
-      db.run(
-        `INSERT INTO apikeys (email, key) VALUES (?, ?)`,
-        email,
-        apiKey,
-        (err) => {
-          if (err) {
-            console.error(err);
-            res.status(500).send({
-              message: "Error generating API key",
-              key: null,
-            });
-          } else {
-            res.status(200).send({
-              message: "API key generated successfully",
-              key: apiKey,
-            });
-          }
-        }
-      );
     }
+    const apiKey = uuidv4();
+    db.run(
+      `INSERT INTO apikeys (email, key) VALUES (?, ?)`,
+      email,
+      apiKey,
+      (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({
+            message: "Error generating API key",
+            key: null,
+          });
+        }
+        res.json({
+          message: "API key generated successfully",
+          key: apiKey,
+        });
+      }
+    );
   });
 });
 
-// Add middleware to verify API key on each request
+// Verify API key on each request
 app.use((req, res, next) => {
   const apiKey = req.headers["x-api-key"];
   db.get(`SELECT * FROM apikeys WHERE key = ?`, apiKey, (err, row) => {
     if (err) {
       console.error(err);
-      res.status(500).send("Error verifying API key");
-    } else if (!row) {
-      res.status(401).send("Invalid API key");
-    } else {
-      next();
+      return res.status(500).send("Error verifying API key");
     }
+    if (!row) {
+      return res.status(401).send("Invalid API key");
+    }
+    next();
   });
 });
 
+// Retrieve location and weather data
 app.get("/", async ({ query }, res) => {
   const { latitude, longitude } = query;
-  const [location, currentWeather, futureWeather] = await Promise.all([
-    getLocationData(latitude, longitude),
-    getCurrentWeatherData(latitude, longitude),
-    getFutureWeatherData(latitude, longitude),
-  ]);
-  res.status(200).send({
-    location,
-    currentWeather: currentWeather ? currentWeather.data : null,
-    futureWeather: futureWeather ? futureWeather.data : null,
-  });
+  try {
+    const [location, currentWeather, futureWeather] = await Promise.all([
+      getLocationData(latitude, longitude),
+      getCurrentWeatherData(latitude, longitude),
+      getFutureWeatherData(latitude, longitude),
+    ]);
+    res.json({
+      location,
+      currentWeather: currentWeather?.data || null,
+      futureWeather: futureWeather?.data || null,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error retrieving data");
+  }
 });
 
 app.listen(PORT, () => {
