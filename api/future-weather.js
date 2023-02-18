584ed6c448ba514cf4975d7d8f1b77c04ac3f76e
Chase Harrison <chaseh88@gmail.com>
2023-02-18 06:05:33 -0600

    update current weather

--- api/future-weather.js
+++ api/future-weather.js
@@ -17,19 +17,15 @@ const apiKey = "47b4b166bf68465eb7c4695bd5f4e6f5";
  * @param {number} latitude
  * @param {number} longitude
  */
-const getCurrentWeatherData = async (latitude, longitude) =>
-  withCache(`current-weather-${latitude},${longitude}`, async () => {
+const getFutureWeatherData = async (latitude, longitude) =>
+  withCache(`future-weather-${latitude},${longitude}`, async () => {
     const response = await axiosInstance.get(
-      `current?lat=${latitude}&lon=${longitude}&key=${apiKey}`
+      `forecast/daily?lat=${latitude}&lon=${longitude}&key=${apiKey}`
     );
     return transformData(response.data.data[0]);
   });
 
-const celsiusToFahrenheit = (celsius) =>
-  Math.round((celsius * 9) / 5 + 32).toFixed(2);
-const centimetersToInches = (valueInCm) =>
-  Math.round(valueInCm / 2.54).toFixed(2);
-const msToMph = (valueInMs) => Math.round(valueInMs * 2.237);
+const celsiusToFahrenheit = (celsius) => (celsius * 9) / 5 + 32;
 
 const transformData = (weatherData) => {
   const result = {};
@@ -37,7 +33,9 @@ const transformData = (weatherData) => {
     celsiusToFahrenheit(weatherData.app_temp)
   )} °F`;
   result["air_quality_index"] = weatherData.aqi;
+  result["city"] = weatherData.city_name;
   result["cloud_cover"] = `${weatherData.clouds}%`;
+  result["country"] = weatherData.country_code;
   result["date_time"] = weatherData.datetime;
   result["dew_point"] = `${Math.round(
     celsiusToFahrenheit(weatherData.dewpt)
@@ -46,14 +44,16 @@ const transformData = (weatherData) => {
   result["direct_normal_irradiance"] = weatherData.dni;
   result["elevation_angle"] = `${weatherData.elev_angle}°`;
   result["global_horizontal_irradiance"] = weatherData.ghi;
-  result["max_wind_gust_speed"] = `${msToMph(weatherData.gust)} mph`;
+  result["max_wind_gust_speed"] = `${weatherData.gust} m/s`;
+  result["latitude"] = weatherData.lat;
+  result["longitude"] = weatherData.lon;
   result["observation_time"] = weatherData.ob_time;
   result["part_of_day"] = weatherData.pod === "n" ? "Night" : "Day";
-  result["precipitation"] = `${centimetersToInches(weatherData.precip)} in`;
+  result["precipitation"] = `${weatherData.precip} mm`;
   result["pressure"] = `${weatherData.pres} hPa`;
   result["relative_humidity"] = `${weatherData.rh}%`;
   result["sea_level_pressure"] = `${weatherData.slp} hPa`;
-  result["snowfall"] = `${centimetersToInches(weatherData.snow)} in`;
+  result["snowfall"] = `${weatherData.snow} cm`;
   result["solar_radiation"] = `${weatherData.solar_rad} W/m²`;
   result["sources"] = weatherData.sources;
   result["state"] = weatherData.state_code;
@@ -66,15 +66,15 @@ const transformData = (weatherData) => {
   result["timezone"] = weatherData.timezone;
   result["unix_timestamp"] = weatherData.ts;
   result["uv_index"] = weatherData.uv;
-  result["visibility"] = `${centimetersToInches(weatherData.vis)} in`;
+  result["visibility"] = `${weatherData.vis} km`;
   result["weather_description"] = weatherData.weather.description;
   result[
     "wind_direction"
   ] = `${weatherData.wind_dir}° ${weatherData.wind_cdir_full}`;
-  result["wind_speed"] = `${msToMph(weatherData.wind_spd)} mph`;
+  result["wind_speed"] = `${weatherData.wind_spd} m/s`;
   return result;
 };
 
 module.exports = {
-  getCurrentWeatherData,
+  getFutureWeatherData,
 };
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { getLocationData } = require("./api/mapbox");
 const { getCurrentWeatherData } = require("./api/currentweather");
+const { getFutureWeatherData } = require("./api/future-weather");
 const app = express();
 const PORT = 4001;
 
@@ -8,6 +9,7 @@ app.get("/", async ({ query }, res) => {
   const { latitude, longitude } = query;
   const location = await getLocationData(latitude, longitude);
   const currentWeather = await getCurrentWeatherData(latitude, longitude);
+  const futureWeather = await getFutureWeatherData(latitude, longitude);
   res.status(200).send({
     location,
     currentWeather,
