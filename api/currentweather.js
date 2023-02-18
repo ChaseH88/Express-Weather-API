4f1023eaea0614e922b651e3782d6c8916534072
Chase Harrison <chaseh88@gmail.com>
2023-02-17 23:09:31 -0600

    add current weather api and testing

---
+++ api/currentweather.js
@@ -0,0 +1,80 @@
+const axios = require("axios");
+const { withCache } = require("../utils/with-cache");
+
+const axiosInstance = axios.create({
+  baseURL: "https://api.weatherbit.io/v2.0/",
+  method: "get",
+  timeout: 10000,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
+});
+
+const apiKey = "47b4b166bf68465eb7c4695bd5f4e6f5";
+
+/**
+ *
+ * @param {number} latitude
+ * @param {number} longitude
+ */
+const getCurrentWeatherData = async (latitude, longitude) =>
+  withCache(`current-weather-${latitude},${longitude}`, async () => {
+    const response = await axiosInstance.get(
+      `current?lat=${latitude}&lon=${longitude}&key=${apiKey}`
+    );
+    return transformData(response.data.data[0]);
+  });
+
+const celsiusToFahrenheit = (celsius) => (celsius * 9) / 5 + 32;
+
+const transformData = (weatherData) => {
+  const result = {};
+  result["feels_like"] = `${Math.round(
+    celsiusToFahrenheit(weatherData.app_temp)
+  )} °F`;
+  result["air_quality_index"] = weatherData.aqi;
+  result["city"] = weatherData.city_name;
+  result["cloud_cover"] = `${weatherData.clouds}%`;
+  result["country"] = weatherData.country_code;
+  result["date_time"] = weatherData.datetime;
+  result["dew_point"] = `${Math.round(
+    celsiusToFahrenheit(weatherData.dewpt)
+  )} °F`;
+  result["direct_horizontal_irradiance"] = weatherData.dhi;
+  result["direct_normal_irradiance"] = weatherData.dni;
+  result["elevation_angle"] = `${weatherData.elev_angle}°`;
+  result["global_horizontal_irradiance"] = weatherData.ghi;
+  result["max_wind_gust_speed"] = `${weatherData.gust} m/s`;
+  result["latitude"] = weatherData.lat;
+  result["longitude"] = weatherData.lon;
+  result["observation_time"] = weatherData.ob_time;
+  result["part_of_day"] = weatherData.pod === "n" ? "Night" : "Day";
+  result["precipitation"] = `${weatherData.precip} mm`;
+  result["pressure"] = `${weatherData.pres} hPa`;
+  result["relative_humidity"] = `${weatherData.rh}%`;
+  result["sea_level_pressure"] = `${weatherData.slp} hPa`;
+  result["snowfall"] = `${weatherData.snow} cm`;
+  result["solar_radiation"] = `${weatherData.solar_rad} W/m²`;
+  result["sources"] = weatherData.sources;
+  result["state"] = weatherData.state_code;
+  result["weather_station"] = weatherData.station;
+  result["sunrise"] = weatherData.sunrise;
+  result["sunset"] = weatherData.sunset;
+  result["temperature"] = `${Math.round(
+    celsiusToFahrenheit(weatherData.temp)
+  )} °F`;
+  result["timezone"] = weatherData.timezone;
+  result["unix_timestamp"] = weatherData.ts;
+  result["uv_index"] = weatherData.uv;
+  result["visibility"] = `${weatherData.vis} km`;
+  result["weather_description"] = weatherData.weather.description;
+  result[
+    "wind_direction"
+  ] = `${weatherData.wind_dir}° ${weatherData.wind_cdir_full}`;
+  result["wind_speed"] = `${weatherData.wind_spd} m/s`;
+  return result;
+};
+
+module.exports = {
+  getCurrentWeatherData,
+};
--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const { getLocationData } = require("./api/mapbox");
+const { getCurrentWeatherData } = require("./api/currentweather");
 const app = express();
 const PORT = 4001;
 
 app.get("/", async ({ query }, res) => {
   const { latitude, longitude } = query;
   const location = await getLocationData(latitude, longitude);
+  const currentWeather = await getCurrentWeatherData(latitude, longitude);
   res.status(200).send({
     location,
-    currentWeather: {
-      temperature: 70,
-      summary: "Partly Cloudy",
-    },
+    currentWeather,
   });
 });
 
